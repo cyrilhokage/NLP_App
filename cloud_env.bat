55a847680eeecd2b52f8d4ba1edc3b14102a2b78
Cyrille Nouboue <cyrille.nouboue@groupe-esigelec.org>
2019-04-17 03:55:25 +0200

    add all the project

---
+++ cloud_env.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+CLS
+SET PATH=C:\Users\cyril\Desktop\Personel & Affaires\Learning code\NLP_App\google-cloud-sdk\bin;%PATH%;
+cd C:\Users\cyril\Desktop\Personel & Affaires\Learning code\NLP_App
+ECHO Welcome to the Google Cloud SDK! Run "gcloud -h" to get the list of available commands.
+ECHO ---
+ECHO ON
